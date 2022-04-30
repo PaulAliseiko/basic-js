ee32f3f1aab987b0ac3c38e22386ed409266a534
PaulAliseiko <87947387+PaulAliseiko@users.noreply.github.com>
2022-04-30 23:00:04 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  let [disN, disP, dbN, dbP] = ['--discard-next', '--discard-prev', '--double-next', '--double-prev']
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+  let result = []
+  for(let i = 0; i < arr.length; i++) {
+      result.push(arr[i])
+      console.log(result);
+    
+    if (arr[i] === disN) {
+      if (arr.indexOf(disN) === (arr.length - 1)) {
+        result.pop(arr[i])
+        continue
+      }
+      result.pop(arr[i])
+      i+=2
+    }
+    else if (arr[i] === disP) {
+      if (arr.indexOf(disP) === 0) {
+        result.pop(arr[i])
+        continue
+      }
+      result.pop(arr[i])
+      result.pop(arr[i - 1])
+    }
+    else if (arr[i] === dbN) {
+      if (arr.indexOf(dbN) === (arr.length - 1)) {
+        result.pop(arr[i])
+        continue
+      }
+      result.pop(arr[i])
+      result.push(arr[i + 1])
+    }
+    else if (arr[i] === dbP) {
+      if (arr.indexOf(dbP) === 0) {
+        result.pop(arr[i])
+        continue
+      }
+      result.pop(arr[i])
+      result.push(arr[i - 1])
+    }
+  }  
+  return result
 }
 
 module.exports = {
